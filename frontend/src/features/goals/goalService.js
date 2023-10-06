3f3ffd2a8e7f200a9031c52a09d13880e4775911
nthskyradiated <andy.pandaan@outlook.com>
2023-10-06 23:13:23 +0400

    fix on deleteGoal extrareducer

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -25,9 +25,22 @@ const getGoals = async (token) => {
     return response.data
 }
 
+//Delete User Goal
+const deleteGoal = async (goalId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.delete(API_URL + goalId, config)
+    return response.data
+}
+
 const goalService = {
     createGoal,
     getGoals,
+    deleteGoal,
 }
 
 export default goalService
